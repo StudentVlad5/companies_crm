5bbf121a2a34da75d19b148c194f17d82d493d0f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-06-26 14:25:13 +0300

    add modal and forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import AddCompanyButton from "@/components/add-company-button";
 import StatusLabel, { Status } from "../components/statusLabel"
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home Page</h1>
+      <h1 className="text-xl">{new Date().toLocaleString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
     </main>
   );
 }
