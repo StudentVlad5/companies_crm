eabd4b8ce54391dc45423e977c5797195717f654
Vlad <vlad_np@ukr.net>
2024-09-15 12:13:49 +0200

    finish

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,12 @@
-import Header from '@/components/header';
 import React from 'react';
-
+import Header from '@/app/components/header';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
+    <>
       <Header>Dashboard</Header>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import AddCompanyButton from "@/components/add-company-button";
-import StatusLabel, { Status } from "../components/statusLabel"
-import MagicButton from "@/components/magic-button";
-
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home Page</h1>
-      <h1 className="text-xl">{new Date().toLocaleString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-      <AddCompanyButton />
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
